742a33a50be6a544dcaa2d143d8883cf843abccb
Felfit <31731191+Felfit@users.noreply.github.com>
2018-05-26 12:35:29 +0100

    FUCK SHELL20

--- POO Pratico/menu/__SHELL20.java
+++
@@ -1,8 +0,0 @@
-package menu;
-public class __SHELL20 extends bluej.runtime.Shell {
-public static void run() throws Throwable {
-
-java.lang.String[] __bluej_param0 = { };
-menu.Main.main(__bluej_param0);
-
-}}
